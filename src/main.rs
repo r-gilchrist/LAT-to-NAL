e11154b8a4a9d78f9193a297207c4bf14f05772c
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2022-05-14 14:38:56 +0100

    Added user input functionality
    The program retrieves latitude and longitude values (decimal degrees) from user input, and prints them to screen.
    
    Also includes conversion from string to float.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
+use std::io;
+
 fn main() {
     
     // Include the conversion table as text
     let data = include_str!("NALLAT18.csv");
+
+    // User input variables
+    let mut lat = String::new();
+    let mut lon = String::new();
+
+    // Read latitude from command line
+    println!("Enter latitude (decimal degrees):");
+    io::stdin()
+        .read_line(&mut lat)
+        .expect("Could not read line");
+
+    // Read longitude from command line
+    println!("Enter longitude (decimal degrees):");
+    io::stdin()
+        .read_line(&mut lon)
+        .expect("Could not read line");
+
+    // Latitude and longitude should be floats
+    let lat: f64 = lat.trim().parse().expect("Please enter a number.");
+    let lon: f64 = lon.trim().parse().expect("Please enter a number.");
+
+    // Summarise the user input
+    println!("You entered {}°N, {}°E", &lat, &lon);
 
 }
