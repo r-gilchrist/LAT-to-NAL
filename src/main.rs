c8ddcdfc8ab0ce7e7a07ce6fed4547af45b96fd8
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2022-05-14 15:37:13 +0100

    Added rest of program function
    Added nearest distance calculation and printout function for the user. Program now gives the user the conversion factor from LAT to NAL for the nearest grid cell point from their input coordinates.

--- src/main.rs
+++ src/main.rs
@@ -28,4 +28,39 @@ fn main() {
     // Summarise the user input
     println!("You entered {}°N, {}°E", &lat, &lon);
 
+    // Initial lowest distance
+    let mut lowest_distance: f64 = 180.0;
+    let mut conversion: f64 = -999.0;
+
+    // Loop over each line in the data and parse values
+    for line in data.split("\n") {
+
+        // Get vector of comman-separated values
+        let values: Vec<&str> = line.split(",").collect();
+
+        if values.len() == 3 {
+
+
+            // Get the latitude and longitude
+            let lat_test: f64 = values[0].trim().parse().unwrap();
+            let lon_test: f64 = values[1].trim().parse().unwrap();
+
+            let diff_lat: f64 = &lat_test - &lat;
+            let diff_lon: f64 = &lon_test - &lon;
+
+            let distance = &diff_lat * &diff_lat + &diff_lon * &diff_lon;
+            let distance = distance.powf(0.5);
+
+            if distance < lowest_distance {
+                conversion = values[2].trim().parse().unwrap();
+                lowest_distance = distance;
+            }
+
+        }
+
+    }
+
+    println!("Conversion from NAL to LAT is {}m", conversion);
+    println!("(nearest grid point was {:.4}° away)", lowest_distance);
+
 }
