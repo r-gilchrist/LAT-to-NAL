0dc07da53c1a975186dc7ff5d444abf010646a0f
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2022-05-15 17:35:32 +0100

    Loop over functionality
    Program now loops infinitely until the user closes the command line, so that the program does not need to be re-run where multiple locations are required by the user.

--- src/main.rs
+++ src/main.rs
@@ -5,64 +5,71 @@ fn main() {
     // Include the conversion table as text
     let data = include_str!("NALLAT18.csv");
 
-    // User input variables
-    let mut lat = String::new();
-    let mut lon = String::new();
+    // Infinite loop so users can repeatedly use the program
+    loop {
 
-    // Read latitude from command line
-    println!("Enter latitude (decimal degrees):");
-    io::stdin()
-        .read_line(&mut lat)
-        .expect("Could not read line");
+        // User input variables
+        let mut lat = String::new();
+        let mut lon = String::new();
 
-    // Read longitude from command line
-    println!("Enter longitude (decimal degrees):");
-    io::stdin()
-        .read_line(&mut lon)
-        .expect("Could not read line");
+        // Read latitude from command line
+        println!("Enter latitude (decimal degrees):");
+        io::stdin()
+            .read_line(&mut lat)
+            .expect("Could not read line");
 
-    // Latitude and longitude should be floats
-    let lat: f64 = lat.trim().parse().expect("Please enter a number.");
-    let lon: f64 = lon.trim().parse().expect("Please enter a number.");
+        // Read longitude from command line
+        println!("Enter longitude (decimal degrees):");
+        io::stdin()
+            .read_line(&mut lon)
+            .expect("Could not read line");
 
-    // Summarise the user input
-    println!("You entered {}°N, {}°E", &lat, &lon);
+        // Latitude and longitude should be floats
+        let lat: f64 = lat.trim().parse().expect("Please enter a number.");
+        let lon: f64 = lon.trim().parse().expect("Please enter a number.");
 
-    // Initial lowest distance
-    let mut lowest_distance: f64 = 180.0;
-    let mut conversion: f64 = -999.0;
+        // Summarise the user input
+        println!("You entered {}°N, {}°E", &lat, &lon);
 
-    // Loop over each line in the data and parse values
-    for line in data.split("\n") {
+        // Initial lowest distance
+        let mut lowest_distance: f64 = 180.0;
+        let mut conversion: f64 = -999.0;
 
-        // Get vector of comman-separated values
-        let values: Vec<&str> = line.split(",").collect();
+        // Loop over each line in the data and parse values
+        for line in data.split("\n") {
 
-        // Check to ensure a valid line is being read
-        if values.len() == 3 {
+            // Get vector of comman-separated values
+            let values: Vec<&str> = line.split(",").collect();
 
-            // Get the latitude and longitude
-            let lat_test: f64 = values[0].trim().parse().unwrap();
-            let lon_test: f64 = values[1].trim().parse().unwrap();
+            // Check to ensure a valid line is being read
+            if values.len() == 3 {
 
-            // Calculate difference in longitude and latitude
-            let diff_lat: f64 = &lat_test - &lat;
-            let diff_lon: f64 = &lon_test - &lon;
+                // Get the latitude and longitude
+                let lat_test: f64 = values[0].trim().parse().unwrap();
+                let lon_test: f64 = values[1].trim().parse().unwrap();
 
-            // Calculate overall distance away
-            let distance = &diff_lat * &diff_lat + &diff_lon * &diff_lon;
-            let distance = distance.powf(0.5);
+                // Calculate difference in longitude and latitude
+                let diff_lat: f64 = &lat_test - &lat;
+                let diff_lon: f64 = &lon_test - &lon;
 
-            // Update the conversion factor if the lowest distance beats
-            // the previous best value
-            if distance < lowest_distance {
-                conversion = values[2].trim().parse().unwrap();
-                lowest_distance = distance;
+                // Calculate overall distance away
+                let distance = &diff_lat * &diff_lat + &diff_lon * &diff_lon;
+                let distance = distance.powf(0.5);
+
+                // Update the conversion factor if the lowest distance beats
+                // the previous best value
+                if distance < lowest_distance {
+                    conversion = values[2].trim().parse().unwrap();
+                    lowest_distance = distance;
+                }
             }
         }
+
+        // Print results to screen
+        println!("Conversion from NAL to LAT is {}m", &conversion);
+        println!("(nearest grid point was {:.4}° away)", &lowest_distance);
+
     }
 
-    // Print results to screen
-    println!("Conversion from NAL to LAT is {}m", conversion);
-    println!("(nearest grid point was {:.4}° away)", lowest_distance);
+    
 }
